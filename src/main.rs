4c30114edcb1f24b876d3ca8825c51a0fe3e62ee
prushton2 <peterrushton418@gmail.com>
2024-03-25 23:27:31 -0400

    added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-mod process_attacher;
-
-// use crate::process_attacher;
+use gdb_mi;
 
 fn main() {
-    let r = process_attacher::exec("./a.out");
-    println!("{:?}", r);
+    println!("init session");
+    let mut session = match gdb_mi::Session::new("gdb", &["./main".to_string()]) {
+        Ok(c) => c,
+        Err(_c) => return,
+    };
+    println!("making breakpoint");
+
+    //set bp
+    let bp_create = match session.send("b 1\n") {
+        Ok(c) => c,
+        Err(c) => {println!("Err: {:?}", c); return;}
+    };
+
+
+    println!("Running");
+    let run = match session.send("r\n") {
+        Ok(c) => c,
+        Err(c) => {println!("Err: {:?}", c); return;}
+    };
+
+
+
+
+    println!("receive breakpoint");
+
+    let bp_create_rec = match session.receive() {
+        Ok(c) => c,
+        Err(c) => {println!("Err: {:?}", c); return;}
+    };
+
+    println!("{:?}", bp_create_rec);
+
+    println!("aa");
 }
