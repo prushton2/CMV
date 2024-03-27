ac7268a4cde834d0d8e38091915b985cef5a40a1
prushton2 <peterrushton418@gmail.com>
2024-03-27 00:12:18 -0400

    fix

--- src/main.py
+++ src/main.py
@@ -1,35 +1,37 @@
 #!/bin/bython
 import pexpect
+import re
 
 analyzer = pexpect.spawn('gdb', encoding='utf-8')
-analyzer.expect(r'\(gdb\) ')
-
+analyzer.expect(r"\(gdb\) ")
+print(analyzer.before)
 
 ipt = input("Enter executable file:\n> ")
 
-analyzer.sendline("file "+ipt);
-analyzer.expect(r'\(gdb\) ')
+analyzer.sendline("\nfile "+ipt);
 
+analyzer.expect(r"\(gdb\) ")
 analyzer.sendline(f"b 1\n");
-analyzer.expect(r'\(gdb\) ')
 
+analyzer.expect(r'\(gdb\) ')
 analyzer.sendline(f"r\ny\n");
-analyzer.expect(r'\(gdb\) ')
 
-while(ipt != "q") {
-    
+
+while(1) {
+    ipt = input("> ");
     #print("TRYING:: Trying", ipt, "...")
+    analyzer.expect(r"\(gdb\) ")
     analyzer.sendline(ipt)
-    if(ipt == "r") {
-        analyzer.sendline("y");
-    }
-    analyzer.expect(r"\(gdb\) ")
-    print(analyzer.before)
-
-    analyzer.sendline("info locals");
-    analyzer.expect(r"\(gdb\) ")
-    print(analyzer.before)
+    out = (analyzer.before)
     
-    ipt = input(">");
+   if re.search(r"[0-9]*\s*free\(.*\)", out) {
+       print("Free statement present here");
+   }
+
+   print(out)
+
+   #analyzer.sendline("info locals");
+   #analyzer.expect(r"\(gdb\) ")
+   ##print(analyzer.before)   
 }
 print("Done");
